1ad51820262aa4e5199bb2cb300bf9b17c689bba
fudev1 <matteo.divita@outlook.be>
2023-08-18 16:09:58 +0200

    connexion bot > db

--- API-Bot-Discord/app.js
+++ API-Bot-Discord/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const server = express();
 const port = "8002";
 const { versionRouter } = require('./Routers/version.router');
+const { citationsRouter } = require('./Routers/citations.router');
 
-
+// Mes liens Routers
 server.use(versionRouter);
-
+server.use(citationsRouter);
 
 server.listen(port, () => {
     console.log(`Serveur à l'écoute sur ${port}`);
