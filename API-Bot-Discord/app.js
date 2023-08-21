a3c06879ff85782bf10797f18052595e2b819705
fudev1 <matteo.divita@outlook.be>
2023-08-21 14:19:54 +0200

    static

--- API-Bot-Discord/app.js
+++ API-Bot-Discord/app.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const { citationsRouter } = require('./Routers/citations.router');
-const { forbiddenRouter } = require('./Routers/forbidden.router');
-const { dashRouter } = require('./Routers/dashboard.router');
-const { secretMiddleware } = require('./Middlewares/secretMiddleWare');
+const express = require("express");
+const { citationsRouter } = require("./Routers/citations.router");
+const { forbiddenRouter } = require("./Routers/forbidden.router");
+const { dashRouter } = require("./Routers/dashboard.router");
+const { secretMiddleware } = require("./Middlewares/secretMiddleWare");
 port = "8080";
 
 const server = express();
 
 server.use(citationsRouter);
-server.use('/dashboard', secretMiddleware)
-server.use('/dashboard', dashRouter);
-server.use('/forbidden', forbiddenRouter);
-
+server.use("/dashboard", secretMiddleware);
+server.use("/dashboard", dashRouter);
+server.use("/forbidden", forbiddenRouter);
 
 server.listen(port, () => {
-    console.log(`Serveur ok sur le port ${port}`);
+  console.log(`Serveur ok sur le port ${port}`);
 });
