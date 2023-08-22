cf9c5c878143becbe56240746d811070e9080546
fudev1 <matteo.divita@outlook.be>
2023-08-22 15:55:38 +0200

    gestion user

--- API-Bot-Discord/Middlewares/secretMiddleWare.js
+++ API-Bot-Discord/Middlewares/secretMiddleWare.js
@@ -1,9 +1,9 @@
-async function secretMiddleware( request, response, next) {
-    const { secret } = request.query;
-    if (secret !== 'svp') {
-        return response.redirect('/forbidden');
-    }
-    next()
+async function secretMiddleware(request, response, next) {
+  const { user } = request.query;
+  if (user !== "mdv") {
+    return response.redirect("/forbidden");
+  }
+  next();
 }
 
-exports.secretMiddleware = secretMiddleware
+exports.secretMiddleware = secretMiddleware;
